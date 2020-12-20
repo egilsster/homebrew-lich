0d8546ee5af7e7d27acaebc97d92409aeefd4882
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2020-12-20 15:19:38 +0100

    chore: release v0.1.1

--- Formula/lich.rb
+++ Formula/lich.rb
@@ -1,9 +1,9 @@
 class Lich < Formula
     desc "A license checker"
     homepage "https://github.com/egilsster/lich-rs"
-    url "https://github.com/egilsster/lich-rs/releases/latest/download/lich-mac.tar.gz"
-    sha256 "ac2ddcfed75ed1a6835e14f70caa1d73197a93654ed4fc4da575656412597ec3"
-    version "0.1.0"
+    url "https://github.com/egilsster/lich-rs/releases/latest/download/lich-x86_64-apple-darwin.tar.gz"
+    sha256 "c126ee1df691857967f51a5645fe4e162679dfd44a2327360d6b030f78b7c7a0"
+    version "0.1.1"
 
     def install
       bin.install "lich"
